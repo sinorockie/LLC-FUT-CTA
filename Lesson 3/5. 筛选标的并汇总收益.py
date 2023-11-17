9f2c27a84b301437a0ef01b94002cd7fa837985c
Lei Shi <sinorockie@users.noreply.github.com>
2023-11-17 11:09:04 +0800

    update

--- Lesson 3/5. 筛选标的并汇总收益.py
+++ Lesson 3/5. 筛选标的并汇总收益.py
@@ -1,5 +1,6 @@
 import datetime
 
+import numpy as np
 import pandas as pd
 
 profit_df = pd.read_excel("../output/期货量化实践_Carry收益.xlsx", None)
@@ -16,6 +17,18 @@ for sheet_name in profit_df:
 trade_notional_map = {k: v for k, v in trade_notional_map.items() if not pd.isna(v) and v >= 5000000000}
 
 available_budget = 100000000
+
+# 创建一个空的 DataFrame 其索引为交易日
+multi_index = pd.MultiIndex.from_tuples(
+    [
+        ('', '当日可用资金'),
+        ('', '当日收益'),
+        ('', '年化收益率'),
+        ('', '夏普比率'),
+        ('', 'Calmar Ratio(卡玛比率)')
+    ]
+)
+df = pd.DataFrame(index=trade_date_series, columns=multi_index)
 
 carry_ma10_map = {}
 start_date = trade_date_series[0]
@@ -48,22 +61,54 @@ for i in range(len(trade_date_series)):
             sheet = profit_df[item[0]]
             row = sheet[sheet['日期'] == start_date]
             amount_map[item[0]] = int(each_budget / 4 / 0.15 / row.iloc[0]['策略开仓价'] / row.iloc[0]['合约乘数']) * row.iloc[0]['合约乘数']
-        print(budget_map)
-        print(amount_map)
+            if (item[0], '持仓量') not in df.columns:
+                df[(item[0], '持仓量')] = 0.
+                df[(item[0], '单位收益')] = 0.
+                df[(item[0], '持仓收益')] = 0.
         for key in amount_map.keys():
             sheet = profit_df[key]
             # 取日期在start_date与adjust_date之间的行
-            rows = sheet[(sheet['日期'] >= start_date) & (sheet['日期'] < adjust_date)]
+            rows = sheet[(sheet['日期'] >= start_date) & (sheet['日期'] <= adjust_date)]
+            for index in rows['日期']:
+                df.loc[index, (key, '持仓量')] = amount_map[key] if rows[rows['日期'] == index]['最优价格'].values[0] > 0 else 0.
+                df.loc[index, (key, '单位收益')] = rows[rows['日期'] == index]['策略收益'].values[0]
+                df.loc[index, (key, '持仓收益')] = rows[rows['日期'] == index]['策略收益'].values[0] * amount_map[key]
             # 累计盈亏求和
             profit = rows['策略收益'].sum()
             available_budget += (profit * amount_map[key])
+        print(budget_map)
+        print(amount_map)
         print(available_budget)
     else:
         if i == len(trade_date_series) - 1:
             break
         else:
-            if trade_date_series[i+1] > adjust_date:
-                start_date = trade_date_series[i+1]
+            if trade_date_series[i + 1] > adjust_date:
+                start_date = trade_date_series[i + 1]
                 adjust_date = start_date + datetime.timedelta(days=30)
                 budget_map = {}
                 amount_map = {}
+
+# 当日收益等于所有持仓收益之和
+df[('', '当日收益')] = df.xs('持仓收益', axis=1, level=1).sum(axis=1)
+# 第一天初始化可用资金为100000000
+df.iloc[0, 0] = 100000000
+# 每一行当日可用资金等于前一日可用资金加上前一日收益
+# 计算年化收益率 年化收益率=(期末净值/期初净值)^(1/年数)-1
+for i in range(len(df)):
+    if i == len(df) - 1:
+        print()
+    if i > 0:
+        df.iloc[i, 0] = df.iloc[i - 1, 0] + df.iloc[i - 1, 1]
+    # 取前252个交易日的数据 不足252个交易日则取所有交易日的数据
+    rows = df.iloc[max(0, i - 252):(i+1), :]
+    profit = rows.iloc[:, 1].sum()
+    budget = rows.iloc[0, 0]
+    df.iloc[i, 2] = profit / budget
+    # 计算夏普比率 夏普比率=(年化收益率-无风险利率)/年化收益率的标准差
+    df.iloc[i, 3] = df.iloc[i, 2] / rows.iloc[:, 2].std()
+    # 计算Calmar Ratio(卡玛比率) Calmar Ratio(卡玛比率)=年化收益率/最大回撤
+    if i > 0:
+        df.iloc[i, 4] = df.iloc[i, 2] / (rows.iloc[:, 0].max() - rows.iloc[:, 0].min()) * rows.iloc[:, 0].max()
+
+df.to_excel('../output/期货量化实践_策略收益.xlsx', sheet_name='策略收益')
